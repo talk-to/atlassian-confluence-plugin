417d58603fc2999d3a4d2c8226b0b87b1f20268f
AB <ab@BTMDVABHINAVB.local>
2019-08-01 11:23:37 +0530

    Bumped version to 1.0.0, disable logs.

--- src/main/java/com/flock/app/Logger.java
+++ src/main/java/com/flock/app/Logger.java
@@ -2,6 +2,6 @@ package com.flock.app;
 
 public class Logger {
     public static void println(String str) {
-        System.out.println("Abhinav: " + str);
+//        System.out.println("Abhinav: " + str);
     }
 }
