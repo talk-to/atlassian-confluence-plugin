003286f592c726fc536e40ddd91f3d0d9b66feb0
Abhinav Bhadoria <49869439+abhinav-flock@users.noreply.github.com>
2022-11-28 17:28:28 +0530

    Migrate confluence 7.20.2 (#3)
    * Bump confluence ver to 7.20.2
    
    https://confluence.atlassian.com/support/atlassian-support-end-of-life-policy-201851003.html
    
    Confluence 7.20.2 EOL Date is 04-10-2024
    
    * Bump version to 1.0.1
    
    * Add confluence, plugin version in payload
    
    Co-authored-by: abhinavb <abhinav.bhadoria1594@gmail.com>

--- src/main/java/com/flock/app/network/MyNetworkClientImpl.java
+++ src/main/java/com/flock/app/network/MyNetworkClientImpl.java
@@ -2,13 +2,18 @@ package com.flock.app.network;
 
 import com.atlassian.plugin.spring.scanner.annotation.export.ExportAsService;
 import com.flock.app.ConfluenceActionType;
+import com.flock.app.Constants;
 import com.flock.app.Logger;
 import com.flock.app.settings.BaseUrlStore;
 import com.goebl.david.Webb;
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import static com.flock.app.Constants.CONFLUENCE_VERSION;
+import static com.flock.app.Constants.PLUGIN_VERSION;
 
 @ExportAsService({MyNetworkClient.class})
 @Named
@@ -32,7 +37,7 @@ public class MyNetworkClientImpl implements MyNetworkClient {
         if (baseUrlStore.get() == null) return;
 
         try {
-            String body = gson.toJson(toSend);
+            String body = gson.toJson(decorateWithGlobalProps(toSend));
             webb.post(baseUrlStore.get())
                     .header(HEADER_CONFLUENCE_BASE_URL, baseUrlStore.getConfluenceBaseUrl())
                     .header(HEADER_ACTION_TYPE, confluenceActionType.getNameString())
@@ -43,5 +48,12 @@ public class MyNetworkClientImpl implements MyNetworkClient {
             Logger.println("Serialized Object: " + body);
         } catch (Exception ignored) {
         }
+    }
+
+    private JsonElement decorateWithGlobalProps(Object object) {
+        JsonElement jsonElement = gson.toJsonTree(object);
+        jsonElement.getAsJsonObject().addProperty("confluenceVersion", CONFLUENCE_VERSION);
+        jsonElement.getAsJsonObject().addProperty("pluginVersion", PLUGIN_VERSION);
+        return jsonElement;
     }
 }
