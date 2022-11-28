71cc48865f11c5ea07d5c41141e52b2d71278e5d
abhinavb <abhinav.bhadoria1594@gmail.com>
2022-11-28 17:17:30 +0530

    Add confluence, plugin version in payload

--- src/main/java/com/flock/app/Constants.java
+++ src/main/java/com/flock/app/Constants.java
@@ -1,6 +1,10 @@
 package com.flock.app;
 
+import static com.atlassian.confluence.util.GeneralUtil.getVersionNumber;
+
 public class Constants {
-    private static final String PROJECT_PREFIX = "flock-for-confluence/";
-    public static final String KEY_WEB_HOOK_URL = PROJECT_PREFIX + "key-web-hook-url";
+  public static final String CONFLUENCE_VERSION = getVersionNumber();
+  public static final String PLUGIN_VERSION = "1.0.1";
+  private static final String PROJECT_PREFIX = "flock-for-confluence/";
+  public static final String KEY_WEB_HOOK_URL = PROJECT_PREFIX + "key-web-hook-url";
 }
